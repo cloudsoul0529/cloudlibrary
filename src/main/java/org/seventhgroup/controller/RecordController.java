45323a6cc18ea750c92d6d8c2d6004ca03c634b7
cloudsoul <z2714589104@outlook.com>
2025-11-16 18:02:23 +0800

    Controller层

---
+++ src/main/java/org/seventhgroup/controller/RecordController.java
@@ -0,0 +1,11 @@
+package org.seventhgroup.controller;
+/**
+ * @author cloudsoul-ZX
+ * @time 2025.11.16
+ * */
+public class RecordController {
+    //查询借阅记录
+    public void searchRecords(){
+
+    }
+}
