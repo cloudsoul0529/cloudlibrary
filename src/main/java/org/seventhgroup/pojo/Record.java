48a57b33a1b311197a1c1c191426bbc0e7a0e486
sooooya838 <3461611541@qq.com>
2025-12-10 21:11:45 +0800

    借阅记录

--- src/main/java/org/seventhgroup/pojo/Record.java
+++ src/main/java/org/seventhgroup/pojo/Record.java
@@ -1,63 +1,39 @@
 package org.seventhgroup.pojo;
 
 import java.io.Serializable;
+
 /**
- * @author
- * @time 2025.11.17
- * */
+ * 借阅记录
+ *
+ * @author sooooya838
+ */
 public class Record implements Serializable {
-    private Integer id;        //图书借阅id
-    private String bookname;   //借阅的图书名称
-    private String bookisbn;   //借阅的图书的ISBN编号
-    private String borrower;   //图书借阅人
-    private String borrowTime; //图书借阅时间
-    private String remandTime; //图书归还时间
 
-    public Integer getId() {
-        return id;
-    }
+    private static final long serialVersionUID = 1L;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    //记录ID
+    private Integer id;
+    //书名
+    private String bookname;
+    //ISBN
+    private String bookisbn;
+    //借阅人
+    private String borrower;
+    //借阅时间
+    private String borrowTime;
+    //归还时间
+    private String remandTime;
 
-    public String getBookname() {
-        return bookname;
-    }
-
-    public void setBookname(String bookname) {
-        this.bookname = bookname;
-    }
-
-    public String getBookisbn() {
-        return bookisbn;
-    }
-
-    public void setBookisbn(String bookisbn) {
-        this.bookisbn = bookisbn;
-    }
-
-    public String getBorrower() {
-        return borrower;
-    }
-
-    public void setBorrower(String borrower) {
-        this.borrower = borrower;
-    }
-
-    public String getBorrowTime() {
-        return borrowTime;
-    }
-
-    public void setBorrowTime(String borrowTime) {
-        this.borrowTime = borrowTime;
-    }
-
-    public String getRemandTime() {
-        return remandTime;
-    }
-
-    public void setRemandTime(String remandTime) {
-        this.remandTime = remandTime;
-    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getBookname() { return bookname; }
+    public void setBookname(String bookname) { this.bookname = bookname; }
+    public String getBookisbn() { return bookisbn; }
+    public void setBookisbn(String bookisbn) { this.bookisbn = bookisbn; }
+    public String getBorrower() { return borrower; }
+    public void setBorrower(String borrower) { this.borrower = borrower; }
+    public String getBorrowTime() { return borrowTime; }
+    public void setBorrowTime(String borrowTime) { this.borrowTime = borrowTime; }
+    public String getRemandTime() { return remandTime; }
+    public void setRemandTime(String remandTime) { this.remandTime = remandTime; }
 }
--- src/main/java/org/seventhgroup/service/RecordService.java
+++ src/main/java/org/seventhgroup/service/RecordService.java
@@ -5,12 +5,28 @@ import org.seventhgroup.pojo.Record;
 import org.seventhgroup.pojo.User;
 
 /**
- * @author DeadSoup
- * @time 2025/11/19 17:22
+ * 借阅记录接口
+ *
+ * @author sooooya838
  */
 public interface RecordService {
-    //新增借阅记录
+
+    /**
+     * 新增借阅记录
+     *
+     * @param record 记录实体
+     * @return 影响行数
+     */
     Integer addRecord(Record record);
-    //查询借阅记录
+
+    /**
+     * 查询借阅记录
+     *
+     * @param record   查询条件
+     * @param user     当前登录用户
+     * @param pageNum  页码
+     * @param pageSize 每页条数
+     * @return 分页结果封装
+     */
     PageResult searchRecords(Record record, User user, Integer pageNum, Integer pageSize);
 }
